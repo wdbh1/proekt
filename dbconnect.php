7c16aeb0b6569eb72116b8a2ea48720f952fe000
wdbh1 <130337152+wdbh1@users.noreply.github.com>
2023-06-10 10:19:09 +0500

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,7 +0,0 @@
-require __DIR__ . '/vendor/autoload.php';
-use Dotenv\Dotenv;                        
-if (file_exists(__DIR__."/.env"))
-{
-    $dotenv = Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
